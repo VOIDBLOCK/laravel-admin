68cedf26a3a8ab3b0decd6a9faa5f7eb86ab8952
Mo. Gamil <gemy.dev@gmail.com>
2018-09-01 00:04:28 +0200

    #FIXES

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,8 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
+        {{-- <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!} --}}
+        @version
     </div>
     <!-- Default to the left -->
     <strong>Powered by <a href="https://voidblock.com/" target="_blank">VOIDBLOCK LLC</a></strong>
