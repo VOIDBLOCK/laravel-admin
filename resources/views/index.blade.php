47dd53cdb7e421a1639227ecf8a7a95cf8f0e42e
Mo. Gamil <gemy.dev@gmail.com>
2018-09-09 03:42:19 +0200

    #Fix RTL Missing Class

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale', 'en') }}" class="lang-{{ config('app.locale', 'en') }}">
+<html lang="{{ config('app.locale', 'en') }}" class="lang-{{ config('app.locale', 'en') }} {{ (config('admin.is_rtl', false) ? 'layout-rtl': 'layout-ltr') }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
