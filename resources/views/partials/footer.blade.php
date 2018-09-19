4b0deaf69b3219f93ade9a1cd776b44cf959a35b
Mo. Gamil <gemy.dev@gmail.com>
2018-09-19 16:41:09 +0200

    #Fix Footer Partial View

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,16 +2,10 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        {{-- <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!} --}}
-        @version
-        @endif
-
         &nbsp;&nbsp;&nbsp;&nbsp;
-
-        @if(config('admin.show_version'))
-        <strong>Version</strong>&nbsp;&nbsp; {!! \Encore\Admin\Admin::VERSION !!}
+        @if(config('admin.show_version', true))
+            @version
         @endif
-
     </div>
     <!-- Default to the left -->
     <strong>Powered by <a href="https://voidblock.com/" target="_blank">VOIDBLOCK LLC</a></strong>
